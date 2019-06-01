a04d5a834a7d66a17a19aa25d829bebb58a9329a
aki <>
2019-06-01 16:14:49 +0900

    [fix]column name.

--- db/migrate/20190601064702_create_menus.rb
+++ db/migrate/20190601064702_create_menus.rb
@@ -1,7 +1,7 @@
 class CreateMenus < ActiveRecord::Migration[5.2]
   def change
     create_table :menus do |t|
-      t.integer :calss_id
+      t.integer :class_id
       t.string :name
 
       t.timestamps

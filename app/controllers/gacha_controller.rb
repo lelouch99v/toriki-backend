2aba9cfb1d3ad922ff5df2654fc5525bafd604ab
aki <>
2019-06-05 06:40:06 +0900

    [up]postできるようになった

--- app/controllers/gacha_controller.rb
+++ app/controllers/gacha_controller.rb
@@ -2,8 +2,8 @@
 
 class GachaController < ApplicationController
   def turn_gacha
-    @food_list = Menu.where('class_id <= 4').order('RANDOM()').limit(1)
-    @drink_list = Menu.where('class_id = 5').order('RANDOM()').limit(1)
+    @food_list = Menu.where('class_id <= 4').order('RANDOM()').limit(params[:numOfFood])
+    @drink_list = Menu.where('class_id = 5').order('RANDOM()').limit(params[:numOfDrink])
     render json: { foodList: @food_list, drinkList: @drink_list }
   end
 end

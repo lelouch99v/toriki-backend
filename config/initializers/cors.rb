970e872d757270da65ac37eefa0ec1430dc29047
aki <>
2019-06-29 12:53:06 +0900

    [up]

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins /localhost\:\d+/, /ak-toriki-frontend\.herokuapp\.com/
       origins /localhost\:\d+/, /ak-toriki-nuxt-frontend\.herokuapp\.com/
-      origins /localhost\:\d+/, /ak-toriki-nuxt-frontend-stg\.herokuapp\.com/
+      # origins /localhost\:\d+/, /ak-toriki-nuxt-frontend-stg\.herokuapp\.com/
 
       resource '*',
         headers: :any,
